c787e81a3619686772d0bcd7452c2bb8d1513c36
Dragon Dave <dragon@scraperwiki.com>
2014-03-17 11:35:01 +0000

    add more dependancies

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='dshelpers',
       version='1.0.3',
       py_modules=['dshelpers'],
-      install_requires=['requests', 'requests_cache', 'mock'],
+      install_requires=['requests', 'requests_cache', 'mock', 'nose', 'scraperwiki'],
      )
