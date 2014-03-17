db16c81ee79e39c039aa371e50399635a6cbf580
Dragon Dave <dragon@scraperwiki.com>
2014-03-17 11:27:17 +0000

    minimal setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,5 @@
-from setuptools import setup, find_packages
-
-long_desc = """
-Provides some helpers functions used by the ScraperWiki Data Services team.
-"""
-# See https://pypi.python.org/pypi?%3Aaction=list_classifiers for classifiers
-
-setup(
-    name='dshelpers',
-    version='1.0.3',
-    description="Extract fields from tabular data with complex expressions.",
-    long_description=long_desc,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-    ],
-    keywords='',
-    author='ScraperWiki Limited',
-    author_email='dataservices@scraperwiki.com',
-    url='http://scraperwiki.com',
-    license='BSD',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=[],
-    include_package_data=False,
-    zip_safe=False,
-    install_requires=[
-        'requests',
-        'requests_cache',
-        'mock',
-    ],
-    tests_require=[],
-    entry_points=\
-    """
-    """,
-)
+from distutils.core import setup
+setup(name='dshelpers',
+      version='1.0.3',
+      py_modules=['dshelpers'],
+     )
