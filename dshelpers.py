508f8e7538ad34e11daf587dbbcbc0d6ab34f952
Steven Maude <git@stevenmaude.co.uk>
2015-06-29 15:22:18 +0100

    Use bytes throughout tests instead of str
    str in Python 3 are unicode strings, not bytes.

--- dshelpers.py
+++ dshelpers.py
@@ -270,7 +270,7 @@ def test_rate_limit_sleeps_up_to_correct_period(mock_sleep):
 def test_passes_headers_through(mock_requests_get):
     fake_response = requests.Response()
     fake_response.status_code = 200
-    fake_response._content = str("Hello")
+    fake_response._content = b"Hello"
     mock_requests_get.return_value = fake_response
     _download_with_backoff('http://fake_url.com', headers={'this':'included'})
     mock_requests_get.assert_called_with(u'http://fake_url.com', headers=CaseInsensitiveDict({u'this': u'included', u'user-agent': u'ScraperWiki Limited (bot@scraperwiki.com)'}), timeout=60)
@@ -280,7 +280,7 @@ def test_passes_headers_through(mock_requests_get):
 def test_override_timeout(mock_requests_get, mock_time_sleep):
     fake_response = requests.Response()
     fake_response.status_code = 200
-    fake_response._content = str("Hello")
+    fake_response._content = b"Hello"
     mock_requests_get.return_value = fake_response
     _download_with_backoff('http://fake_url.com', timeout=10)
     mock_requests_get.assert_called_with(u'http://fake_url.com', headers=CaseInsensitiveDict({u'user-agent': u'ScraperWiki Limited (bot@scraperwiki.com)'}), timeout=10)
@@ -290,18 +290,18 @@ def test_override_timeout(mock_requests_get, mock_time_sleep):
 def test_get_response_object_on_good_site(mock_requests_get, mock_sleep):
     fake_response = requests.Response()
     fake_response.status_code = 200
-    fake_response._content = str("Hello")
+    fake_response._content = b"Hello"
     mock_requests_get.return_value = fake_response
-    assert_equal("Hello", request_url('http://fake_url.com').content)
+    assert_equal(b"Hello", request_url('http://fake_url.com').content)
 
 @patch('time.sleep')
 @patch('dshelpers.requests.get')
 def test_backoff_function_works_on_a_good_site(mock_requests_get, mock_sleep):
     fake_response = requests.Response()
     fake_response.status_code = 200
-    fake_response._content = str("Hello")
+    fake_response._content = b"Hello"
     mock_requests_get.return_value = fake_response
-    assert_equal("Hello", _download_with_backoff('http://fake_url.com').read())
+    assert_equal(b"Hello", _download_with_backoff('http://fake_url.com').read())
 
 
 @patch('time.sleep')
@@ -315,7 +315,7 @@ def test_backoff_function_works_after_one_failure(
 
         good_response = requests.Response()
         good_response.status_code = 200
-        good_response._content = str("Hello")
+        good_response._content = b"Hello"
 
         yield bad_response
         yield bad_response
@@ -325,7 +325,7 @@ def test_backoff_function_works_after_one_failure(
 
     with rate_limit_disabled():
         assert_equal(
-            "Hello",
+            b"Hello",
             _download_with_backoff('http://fake_url.com').read())
 
     assert_equal(
@@ -362,7 +362,7 @@ def test_backoff_raises_on_five_failures(mock_requests_get, mock_sleep):
 def test_handle_socket_timeout(mock_requests_get, mock_sleep):
     fake_response = requests.Response()
     fake_response.status_code = 200
-    fake_response._content = str("Hello")
+    fake_response._content = b"Hello"
     
     mock_requests_get.side_effect = [socket.timeout, fake_response]
     # socket.timeout used to cause an exception.
@@ -373,7 +373,7 @@ def test_handle_socket_timeout(mock_requests_get, mock_sleep):
 def test_download_url_sets_user_agent(mock_requests_get):
     fake_response = requests.Response()
     fake_response.status_code = 200
-    fake_response._content = str("Hello")
+    fake_response._content = b"Hello"
     mock_requests_get.return_value = fake_response
 
     _download_without_backoff('http://invalid')
